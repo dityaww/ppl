ed608452aff2630372644e2c6b49e4c5b6e05595
dityaww <111202012838@mhs.dinus.ac.id>
2023-05-26 20:35:49 +0700

    login-page update

--- views/src/pages/Login.jsx
+++ views/src/pages/Login.jsx
@@ -4,6 +4,7 @@ const Login = () => {
             <section className="h-[100vh] grid grid-cols-2">
                 <div className="">
                     <p>abs</p>
+                    <p>abc</p>
                 </div>
 
                 <div className="bg-slate-200">
