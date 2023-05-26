09e2e2e2a41bcf31fa6fa952e51a49b047c7d1ef
Syaifun Nadhif <nadhifeldrafo@gmail.com>
2023-05-26 19:27:14 +0700

    set up folder

---
+++ controllers/user.js
@@ -0,0 +1,64 @@
+const {User} = require('../db/models');
+const bcryp = require('bcrypt');
+const passport = require('../utils/passport');
+
+module.exports = {
+    registerPage: (req, res) => {
+        return res.render('auth/register', {errors: {name: '', email: '', password: ''}});
+    },
+
+    register: async (req, res) => {
+        try {
+            const {name, email, password} = req.body;
+
+            // validasi
+            const error = {errors: {}};
+            if (!name) {
+                error.errors.name = 'name is required';
+            }
+            if (!email) {
+                error.errors.email = 'email is required';
+            }
+            if (!password) {
+                error.errors.password = 'password is required';
+            }
+            if (!name || !email || !password) {
+                return res.render('auth/register', error);
+            }
+
+            const exist = await User.findOne({where: {email}});
+            if (exist) {
+                error.errors.email = 'email is already used!';
+                return res.render('auth/register', error);
+            }
+
+            const hashPassword = await bcryp.hash(password, 10);
+
+            await User.create({
+                name, email, password: hashPassword
+            });
+
+            return res.redirect('/login');
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    loginPage: (req, res) => {
+        return res.render('auth/login', {errors: {email: '', password: ''}});
+    },
+
+    login: passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    }),
+
+    whoami: async (req, res) => {
+        try {
+            return res.render('auth/whoami', {user: req.user});
+        } catch (error) {
+            throw error;
+        }
+    }
+};
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../controllers/user');
+
+const middlewares = require('../utils/middlewares');
+
+router.get('/', (req, res) => {
+    return res.render('index');
+});
+
+router.get('/home', (req, res) => {
+    return res.render('home');
+});
+
+router.get('/register', user.registerPage);
+router.get('/login', user.loginPage);
+router.post('/auth/register', user.register);
+router.post('/auth/login', user.login);
+router.get('/whoami', middlewares.auth, user.whoami);
+
+module.exports = router;
