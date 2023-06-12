8fe4c9d2c063bafd26da0071a59f7378adecf8ca
dityaww <111202012838@mhs.dinus.ac.id>
2023-06-12 10:55:57 +0700

    uodate route error

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,28 +1,25 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import HomePage from './pages/HomePage'
-import About from './pages/About'
-import Product from './pages/Product'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/product' element={<Product />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
