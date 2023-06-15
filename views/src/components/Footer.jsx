08648c344295e7e49e33252a3ac70a9ca2486b9a
dityaww <111202012838@mhs.dinus.ac.id>
2023-06-15 08:01:43 +0700

    update footer

--- views/src/components/Footer.jsx
+++ views/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="bg-slate-200 p-5">
-      <div className="flex justify-between">
+    <footer className="bg-neutral-700 p-5">
+      <div className="flex justify-between text-neutral-100">
         <div>
           <p>&copy; 2023 Flowbite™. All Rights Reserved.</p>
         </div>
